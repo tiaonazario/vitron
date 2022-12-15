dcd6c01d98dd2e0ef3b1e8ecf8b45498b0cb04ff
tiaonazario <tiaogogo@gmail.com>
2022-12-14 22:54:57 -0300

    chore: create vite structure with react and typescript

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+import vitronLogo from "../assets/images/vitron.svg";
+
+function App() {
+  return (
+    <div>
+      <img src={vitronLogo} className="logo" alt="React logo" />
+    </div>
+  );
+}
+
+export default App;
