3ddbeef5e515e917ea3ed59e1d3bfb7880d3248c
tiaonazario <tiaogogo@gmail.com>
2022-12-16 10:39:14 -0300

    feat: configure the tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: "Roboto, sans-serif",
+      },
+      colors: {
+        dark: {
+          "bg1": "#21252b",
+          "bg2": "#282c34",
+          "bg3": "#2c313c",
+          "bg4": "#2b2f37",
+          "hover": "#414855",
+          "text": "#9da5b4",
+          "title": "#c3c0e3",
+          "active": "#4d78cc",
+          "red": "#FF3A20",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
