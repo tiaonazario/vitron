c47cd46e344b17b1d922addb009cbedc547f5bcf
tiaonazario <tiaogogo@gmail.com>
2022-12-16 15:31:32 -0300

    chore: create configure in the electron with vite

---
+++ electron/main.ts
@@ -0,0 +1,125 @@
+import { app, BrowserWindow, ipcMain, nativeImage, shell } from "electron";
+import { release } from "os";
+import { join } from "path";
+
+process.env.DIST_ELECTRON = join(__dirname, "../");
+process.env.DIST = join(process.env.DIST_ELECTRON, "./dist-electron");
+
+// Disable GPU Accelerator for Windows 7
+if (release().startsWith("6.1")) app.disableHardwareAcceleration();
+
+// Set application name for Windows 10+ notifications
+if (process.platform === "win32") app.setAppUserModelId(app.getName());
+
+if (!app.requestSingleInstanceLock()) {
+  app.quit();
+  process.exit(0);
+}
+
+let win: BrowserWindow | null = null;
+
+// Here, you can also use other preload
+const preload = join(__dirname, "preload.js");
+const url = process.env.VITE_DEV_SERVER_URL;
+const indexHtml = join(process.env.DIST, "index.html");
+
+const createWindow = async () => {
+  const icon = nativeImage.createFromPath(
+    join(app.getAppPath(), "./src/assets/images/vitron.svg")
+  );
+
+  if (app.dock) {
+    app.dock.setIcon(icon);
+  }
+
+  win = new BrowserWindow({
+    title: "Vitron",
+    width: 1080,
+    height: 680,
+    minWidth: 940,
+    minHeight: 560,
+    // frame: false,
+    icon,
+    webPreferences: {
+      //   preload,
+      nodeIntegration: true,
+      contextIsolation: true,
+      devTools: true,
+    },
+  });
+
+  if (process.env.VITE_DEV_SERVER_URL) {
+    win.loadURL(url);
+    // Open devTool if the app is not packaged
+    win.webContents.openDevTools();
+  } else {
+    win.loadFile(indexHtml);
+  }
+
+  // Test actively push message to the Electron-Renderer
+  win.webContents.on("did-finish-load", () => {
+    win?.webContents.send("main-process-message", new Date().toLocaleString());
+  });
+
+  // Make all links open with the browser, not with the application
+  win.webContents.setWindowOpenHandler(({ url }) => {
+    if (url.startsWith("https:")) shell.openExternal(url);
+    return { action: "deny" };
+  });
+
+  // For AppBar
+  ipcMain.on("minimize", () => {
+    // eslint-disable-next-line no-unused-expressions
+    win.isMinimized() ? win.restore() : win.minimize();
+    // or alternatively: win.isVisible() ? win.hide() : win.show()
+  });
+  ipcMain.on("maximize", () => {
+    // eslint-disable-next-line no-unused-expressions
+    win.isMaximized() ? win.restore() : win.maximize();
+  });
+
+  ipcMain.on("close", () => {
+    win.close();
+  });
+};
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  win = null;
+  if (process.platform !== "darwin") app.quit();
+});
+
+app.on("second-instance", () => {
+  if (win) {
+    // Focus on the main window if the user tried to open another
+    if (win.isMinimized()) win.restore();
+    win.focus();
+  }
+});
+
+app.on("activate", () => {
+  const allWindows = BrowserWindow.getAllWindows();
+  if (allWindows.length) {
+    allWindows[0].focus();
+  } else {
+    createWindow();
+  }
+});
+
+// new window example arg: new windows url
+ipcMain.handle("open-win", (event, arg) => {
+  const childWindow = new BrowserWindow({
+    webPreferences: {
+      preload,
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  if (process.env.VITE_DEV_SERVER_URL) {
+    childWindow.loadURL(`${url}#${arg}`);
+  } else {
+    childWindow.loadFile(indexHtml, { hash: arg });
+  }
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,62 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { rmSync } from "fs";
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import electron from "vite-electron-plugin";
+import { customStart, loadViteEnv } from "vite-electron-plugin/plugin";
+import renderer from "vite-plugin-electron-renderer";
+import pkg from "./package.json";
 
-// https://vitejs.dev/config/
+rmSync(path.join(__dirname, "dist-electron"), { recursive: true, force: true });
+
 export default defineConfig({
-  plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.join(__dirname, "src"),
+      styles: path.join(__dirname, "src/styles"),
+    },
+  },
+  plugins: [
+    react(),
+    electron({
+      include: ["electron", "preload"],
+      transformOptions: {
+        sourcemap: !!process.env.VSCODE_DEBUG,
+      },
+      plugins: [
+        ...(process.env.VSCODE_DEBUG
+          ? [
+              // Will start Electron via VSCode Debug
+              customStart(
+                debounce(() => console.log("[startup] Electron App"))
+              ),
+            ]
+          : []),
+        // Allow use `import.meta.env.VITE_SOME_KEY` in Electron-Main
+        loadViteEnv(),
+      ],
+    }),
+    // Use Node.js API in the renderer process
+    renderer({
+      nodeIntegration: true,
+    }),
+  ],
+  server: process.env.VSCODE_DEBUG
+    ? (() => {
+        const url = new URL(pkg.debug.env.VITE_DEV_SERVER_URL);
+        return {
+          host: url.hostname,
+          port: +url.port,
+        };
+      })()
+    : undefined,
+  clearScreen: false,
+});
+
+function debounce<Fn extends (...args: any[]) => void>(fn: Fn, delay = 299) {
+  let t: NodeJS.Timeout;
+  return ((...args) => {
+    clearTimeout(t);
+    t = setTimeout(() => fn(...args), delay);
+  }) as Fn;
+}
